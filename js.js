ccf36d6c14d346e968e7d0aa1cb6ad2216cde449
Vlad <m.obey@bk.ru>
2022-06-27 23:03:27 +0300

    commit

---
+++ js.js
@@ -0,0 +1,7 @@
+let arrow = document.querySelector(".menu__arrow")
+let subArrow = document.querySelector(".sub__arrow")
+let subList = document.querySelector(".sub-menu__List")
+arrow.addEventListener("click", function () {
+	subList.style.display="block";
+
+})
